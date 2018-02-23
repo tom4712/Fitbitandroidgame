5818c2b58bd20adb8df4142ff1802bc126322462
tom4712 <tom4712@naver.com>
2018-02-23 17:21:52 +0900

    앙기모리

--- app/src/main/java/kr/hs/sdh/fitbit/fitbitandroidgame/ShopActivity.java
+++ app/src/main/java/kr/hs/sdh/fitbit/fitbitandroidgame/ShopActivity.java
@@ -50,17 +50,17 @@ public class ShopActivity extends AppCompatActivity {
         height = size.y;
         item_scroll.setLayoutParams(new LinearLayout.LayoutParams(width, height));
 
-        imageView1 = (ImageView) findViewById(R.id.im1);
-        imageView2 = (ImageView) findViewById(R.id.im2);
-        imageView3 = (ImageView) findViewById(R.id.im3);
+        imageView1 = findViewById(R.id.im1);
+        imageView2 = findViewById(R.id.im2);
+        imageView3 = findViewById(R.id.im3);
 
-        imageView4 = (ImageView) findViewById(R.id.im4);
-        imageView5 = (ImageView) findViewById(R.id.im5);
-        imageView6 = (ImageView) findViewById(R.id.im6);
+        imageView4 = findViewById(R.id.im4);
+        imageView5 = findViewById(R.id.im5);
+        imageView6 = findViewById(R.id.im6);
 
-        imageView7 = (ImageView) findViewById(R.id.im7);
-        imageView8 = (ImageView) findViewById(R.id.im8);
-        imageView9 = (ImageView) findViewById(R.id.im9);
+        imageView7 = findViewById(R.id.im7);
+        imageView8 = findViewById(R.id.im8);
+        imageView9 = findViewById(R.id.im9);
         setpadding(width, height);
         Log.d("DB", "asdasdaddsadassadsdasdasdasd");
 
@@ -267,22 +267,31 @@ public class ShopActivity extends AppCompatActivity {
                 finish();
                 break;
             case R.id.set1:
+                checkinner(1,1);
                 break;
             case R.id.set12:
+                checkinner(1,2);
                 break;
             case R.id.set13:
+                checkinner(1,3);
                 break;
             case R.id.set2:
+                checkinner(2,1);
                 break;
             case R.id.set22:
+                checkinner(2,2);
                 break;
             case R.id.set23:
+                checkinner(2,3);
                 break;
             case R.id.set3:
+                checkinner(3,1);
                 break;
             case R.id.set32:
+                checkinner(3,2);
                 break;
             case R.id.set33:
+                checkinner(3,3);
                 break;
         }
     }
@@ -363,7 +372,7 @@ public class ShopActivity extends AppCompatActivity {
         for (int i = 1; i < clothchar.length; i++) {
             if (sex == 0) {
                 if (i >= 1 && i <= 9) {
-                    if (clothchar[i] == '1') {
+                    if (clothchar[i] == '1' || clothchar[i] == '2') {
                         buyvibutton[i - 1].setVisibility(View.GONE);
                         Log.d("DB", "곤곤곤곤곤곤곤곤곤곤" + i);
                         Setvibutton[i - 1].setVisibility(View.VISIBLE);
@@ -373,13 +382,202 @@ public class ShopActivity extends AppCompatActivity {
             } else {
                 if (i == 1 || i == 2 || i == 3 || i == 10 ||
                         i == 11 || i == 12 || i == 13 || i == 14 || i == 15) {
-                    if (clothchar[i] == '1') {
-                        buyvibutton[i - 1].setVisibility(View.INVISIBLE);
+                    if (clothchar[i] == '1' || clothchar[i] == '2') {
+                        buyvibutton[i - 1].setVisibility(View.GONE);
                         Setvibutton[i - 1].setVisibility(View.VISIBLE);
                     }
                 }
             }
         }
+    }
+
+    public void checkinner(int posision,int descript){
+        if(sex == 0){
+            if (posision == 1){
+                if(descript == 1){
+                    if (clothchar[2] == '2'){
+                        clothchar[2] = '1';
+                    }
+                    if (clothchar[3] == '2'){
+                        clothchar[3] = '1';
+                    }
+                    clothchar[1] = '2';
+                }
+                if(descript == 2){
+                    if (clothchar[1] == '2'){
+                        clothchar[1] = '1';
+                    }
+                    if (clothchar[3] == '2'){
+                        clothchar[3] = '1';
+                    }
+                    clothchar[2] = '2';
+                }
+                if(descript == 3){
+                    if (clothchar[1] == '2'){
+                        clothchar[1] = '1';
+                    }
+                    if (clothchar[2] == '2'){
+                        clothchar[2] = '1';
+                    }
+                    clothchar[3] = '2';
+                }
+            }
+            if (posision == 2){
+                if(descript == 1){
+                    if (clothchar[5] == '2'){
+                        clothchar[5] = '1';
+                    }
+                    if (clothchar[6] == '2'){
+                        clothchar[6] = '1';
+                    }
+                    clothchar[4] = '2';
+                }
+                if(descript == 2){
+                    if (clothchar[4] == '2'){
+                        clothchar[4] = '1';
+                    }
+                    if (clothchar[6] == '2'){
+                        clothchar[6] = '1';
+                    }
+                    clothchar[5] = '2';
+                }
+                if(descript == 3){
+                    if (clothchar[4] == '2'){
+                        clothchar[4] = '1';
+                    }
+                    if (clothchar[5] == '2'){
+                        clothchar[5] = '1';
+                    }
+                    clothchar[6] = '2';
+                }
+            }
+            if (posision == 3){
+                if(descript == 1){
+                    if (clothchar[8] == '2'){
+                        clothchar[8] = '1';
+                    }
+                    if (clothchar[9] == '2'){
+                        clothchar[9] = '1';
+                    }
+                    clothchar[7] = '2';
+                }
+                if(descript == 2){
+                    if (clothchar[7] == '2'){
+                        clothchar[7] = '1';
+                    }
+                    if (clothchar[9] == '2'){
+                        clothchar[9] = '1';
+                    }
+                    clothchar[8] = '2';
+                }
+                if(descript == 3){
+                    if (clothchar[7] == '2'){
+                        clothchar[7] = '1';
+                    }
+                    if (clothchar[8] == '2'){
+                        clothchar[8] = '1';
+                    }
+                    clothchar[9] = '2';
+                }
+            }
+        }
+        if(sex == 1){
+            if (posision == 1){
+                if(descript == 1){
+                    if (clothchar[2] == '2'){
+                        clothchar[2] = '1';
+                    }
+                    if (clothchar[3] == '2'){
+                        clothchar[3] = '1';
+                    }
+                    clothchar[1] = '2';
+                }
+                if(descript == 2){
+                    if (clothchar[1] == '2'){
+                        clothchar[1] = '1';
+                    }
+                    if (clothchar[3] == '2'){
+                        clothchar[3] = '1';
+                    }
+                    clothchar[2] = '2';
+                }
+                if(descript == 3){
+                    if (clothchar[1] == '2'){
+                        clothchar[1] = '1';
+                    }
+                    if (clothchar[2] == '2'){
+                        clothchar[2] = '1';
+                    }
+                    clothchar[3] = '2';
+                }
+            }
+            if (posision == 2){
+                if(descript == 1){
+                    if (clothchar[11] == '2'){
+                        clothchar[11] = '1';
+                    }
+                    if (clothchar[12] == '2'){
+                        clothchar[12] = '1';
+                    }
+                    clothchar[10] = '2';
+                }
+                if(descript == 2){
+                    if (clothchar[10] == '2'){
+                        clothchar[10] = '1';
+                    }
+                    if (clothchar[12] == '2'){
+                        clothchar[12] = '1';
+                    }
+                    clothchar[11] = '2';
+                }
+                if(descript == 3){
+                    if (clothchar[10] == '2'){
+                        clothchar[10] = '1';
+                    }
+                    if (clothchar[11] == '2'){
+                        clothchar[11] = '1';
+                    }
+                    clothchar[12] = '2';
+                }
+            }
+            if (posision == 3){
+                if(descript == 1){
+                    if (clothchar[14] == '2'){
+                        clothchar[14] = '1';
+                    }
+                    if (clothchar[15] == '2'){
+                        clothchar[15] = '1';
+                    }
+                    clothchar[13] = '2';
+                }
+                if(descript == 2){
+                    if (clothchar[13] == '2'){
+                        clothchar[13] = '1';
+                    }
+                    if (clothchar[15] == '2'){
+                        clothchar[15] = '1';
+                    }
+                    clothchar[14] = '2';
+                }
+                if(descript == 3){
+                    if (clothchar[13] == '2'){
+                        clothchar[13] = '1';
+                    }
+                    if (clothchar[14] == '2'){
+                        clothchar[14] = '1';
+                    }
+                    clothchar[15] = '2';
+                }
+            }
+        }
+        String input = "";
+        for (int i = 0; i < clothchar.length; i++) {
+            input += Character.toString(clothchar[i]);
+        }
+        db.updateGarments(input);
+        Toast.makeText(this, "창착이 완료 되었습니다!", Toast.LENGTH_SHORT).show();
+        Log.d("DB",""+input);
+
     }
 
 
