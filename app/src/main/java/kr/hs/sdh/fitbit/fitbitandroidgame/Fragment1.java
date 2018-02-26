a6d13a7c7cd7f60c72e7ab398d4f01e89a527fba
Resten <saup19@naver.com>
2018-02-26 22:44:53 +0900

    Merge branch 'master' of D:\newapp3\Fitbitandroidgame with conflicts.

--- app/src/main/java/kr/hs/sdh/fitbit/fitbitandroidgame/Fragment1.java
+++ app/src/main/java/kr/hs/sdh/fitbit/fitbitandroidgame/Fragment1.java
@@ -9,13 +9,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import static kr.hs.sdh.fitbit.fitbitandroidgame.Splash.name;
 
 
 public class Fragment1 extends Fragment {
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_a, container, false);
+        View v =  inflater.inflate(R.layout.fragment_a, container, false);
+        TextView text = v.findViewById(R.id.char_Name);
+        text.setText(Splash.name);
+        return v;
     }
+
+
 }
 
 
