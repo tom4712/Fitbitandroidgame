f46c9e5bd1ffe94c12484f8fbf7d703989ec7a14
tlgj255 <tlgj255@naver.com>
2018-07-12 17:44:16 +0900

    안드로이드에서 바뀌는 설정때문에 일어난 에러 수정

--- openCVLibrary340/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary340/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\Public\\Fitbitandroidgame\\openCVLibrary340\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: D:\\Project\\30420 LSH\\androidProject\\Fitbitandroidgame\\openCVLibrary340\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
